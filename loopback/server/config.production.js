dc529f9cd24db67d32efdad552ee49b6af82e6ef
Richard Good <Richard.Good@ed.ac.uk>
2016-05-05 08:24:26 +0100

    Fix bug with config.production.js which set base url to invalid value if loopback-custom-base-url parameter not set

--- loopback/server/config.production.js
+++ loopback/server/config.production.js
@@ -3,13 +3,16 @@
 var customBaseUrl='';
 process.argv.forEach(function(element){
   var valueArray = element.split('=');
-  if (valueArray[0]='loopback-custom-base-url')
+  if (valueArray[0]=='loopback-custom-base-url')
   {
     customBaseUrl=valueArray[1];
   }
 });
+if (customBaseUrl!='')
+{
+  console.log('Custom api route set to: '+customBaseUrl+'/api');
+}
 
-console.log('Custom api route set to: '+customBaseUrl+'/api');
 module.exports = {
 
   restApiRoot: customBaseUrl+"/api"
