a63c9fd11ec6e3946a21cf3050cbbace5db98f98
Ruben Gamez <ruben.gamez@ed.ac.uk>
2016-05-10 14:00:39 +0100

    Fix custom base url assignment

--- loopback/server/server.js
+++ loopback/server/server.js
@@ -14,7 +14,7 @@ app.start = function() {
   var customBaseUrl='';
   process.argv.forEach(function(element){
     var valueArray = element.split('=');
-    if (valueArray[0]='loopback-custom-base-url')
+    if (valueArray[0] === 'loopback-custom-base-url')
     {
       customBaseUrl=valueArray[1];
     }
