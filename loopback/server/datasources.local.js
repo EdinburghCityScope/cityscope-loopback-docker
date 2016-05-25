b13f5cf74f30fae4a29b88e0929ec76f50c18436
Richard Good <Richard.Good@ed.ac.uk>
2016-05-25 08:26:08 +0100

    Fix in memory datasource to point to the correct relative directory depending on whether it's called via cityscope-data-starter or the main loopback boot file

--- loopback/server/datasources.local.js
+++ loopback/server/datasources.local.js
@@ -1,10 +1,16 @@
 var path = require('path');
 var appDir = path.dirname(require.main.filename);
-
+if (require.main.filename.indexOf("cityscope-data-starter")!=-1)
+{
+  appSuffix = "/loopback/data/loopback-data.json";
+}
+else {
+  appSuffix="/../data/loopback-data.json";
+}
 module.exports={
 
   db: {
     connector: 'memory',
-    file: appDir+'/loopback/data/loopback-data.json',
+    file: appDir+appSuffix,
   }
 };
